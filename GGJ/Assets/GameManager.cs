2504470e63b884cb7bbaec69a823c9c5dbd6b7b8
Okiosk <victorouzaud@gmail.com>
2025-01-26 16:30:29 +0100

    + avancement sur le gamemana

--- GGJ/Assets/GameManager.cs
+++ GGJ/Assets/GameManager.cs
@@ -6,40 +6,61 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public string GameState = "waitStart";
+    private string GameState = "waitStart";
+    [SerializeField]
+    private uiManager ui;
+    [SerializeField]
+    private BubbleManager bubbleMana;
+    [SerializeField]
+    private mapCreation map;
+    [SerializeField]
+    private Pool listebubble;
+
+
+    private int total;
 
     private void Update()
     {
+        Debug.Log(GameState);
         if (GameState == "waitStart")
         {
-
+            ui.afficheStart();
+            if (1==1)
+            {
+                GameState = "starting";
+                ui.enleveStart();
+            }
         }
         if (GameState == "starting")
         {
-
+            
+            bubbleMana.StartGame();
+            GameState = "playing";
         }
         if (GameState == "playing")
         {
-            Pool listebubble = GetComponent<Pool>();
-            mapCreation map = GetComponent<mapCreation>();
-            if (listebubble._objPool.Count == 0)
+            total = 0;
+            foreach (var bubble in listebubble._objPool)
             {
-                GameState = "badend";
-            }
-            else
-            {
-                foreach (var bubble in listebubble._objPool)
+                if (bubble.activeSelf)
                 {
+                    total++;
                     if (bubble.transform.position.x > map.tailleMap)
                     {
                         GameState = "goodend";
                     }
                 }
+                    
             }
+            if (total == 0)
+            {
+                GameState = "badend";
+            }
+            Debug.Log(total);
+
         }
         if (GameState == "goodend")
         {
-            uiManager ui = GetComponent<uiManager>();
             ui.afficheEnd();
         }
         if (GameState == "badend")
