ee96207b0a2066d552f8945e3ebfa1fd562b3196
ElliotN <nedelliot84@gmail.com>
2025-01-26 12:23:19 +0100

    +Camera movement

--- GGJ/Assets/Script/CameraMovement.cs
+++ GGJ/Assets/Script/CameraMovement.cs
@@ -5,4 +5,46 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private Pool _bubblePool;
+    [SerializeField] private Transform _child;
+    private float _yOffset;
+
+    [SerializeField] float _speed;
+
+    private void Start()
+    {
+        _yOffset = Mathf.Abs(transform.position.y - _child.position.y);
+    }
+    private float TargetX()
+    {
+        float sumX = 0;
+        int nbActive = 0;
+        foreach (var bubble in _bubblePool._objPool)
+        {
+            if (bubble.activeSelf)
+            {
+                sumX += bubble.transform.position.x;
+                nbActive++;
+            }
+        }
+        return sumX/nbActive;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float yMove = _child.position.y + _yOffset;
+        float xMove = transform.position.x;
+
+        float targetX = TargetX();
+        if (transform.position.x < targetX)
+            xMove += _speed * Time.deltaTime;
+        else if (transform.position.x > targetX)
+            xMove -= _speed * Time.deltaTime;
+
+        transform.position = new Vector2 (xMove, yMove);
+    }
 }
